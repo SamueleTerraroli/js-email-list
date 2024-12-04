2b4d7e49d3a6872e8de69a114a563d9de74bc3e6
SamueleTerraroli <terraroli99@gmail.com>
2024-12-04 14:27:53 +0100

    feat: add generate email function

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
 //Procedimento logico
 /*
 1. Importo gli elementi di cui ho bisogno
-2. inizializzo
+2. inizializzo, (aggiungo le classi d-none)
 3. Ogni qualvolta clicco il bottone chiamo l'api 10 volte
 4. Pusho le email ricevute in un array
 5. eseeguo una verifica se sono state eseguite tutte le chiamate
 6. se Si faccio comparire la classe d-none al loader e stamp le risposte in lista, se No continuo a pushare
 */
+
+// https://flynn.boolean.careers/exercises/api/random/mail API endpoint
+//step1
+const btn = document.querySelector('.btn');
+const mailList = document.querySelector('ul');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.message-error');
+
+/*dati*/
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
+const cap = 10;
+let emails=[];
+
+init(); //step 1
+
+btn.addEventListener('click', genMails) //step 2
+
+
+
+
+//step 1
+function init(){
+    mailList.classList.add('d-none');
+    loader.classList.add('d-none');
+}
+
+//step3
+function genMails(){
+    emails=[];
+    loader.classList.remove('d-none');
+    mailList.innerHTML = '';
+    error.innerHTML = '';
+
+    for(let i=0; i < cap; i++){
+        axios.get(endpoint)
+            .then(res =>{
+                emails.push(res.data.response) //step 3
+                if(emails.length === cap)/*printFunction*/ //step 4 
+         })
+        .catch(e => {
+            printError(e.message);
+        }) 
+    }
+}

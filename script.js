2af48b07625c90944b9be036900330b89e9e6f5a
SamueleTerraroli <terraroli99@gmail.com>
2024-12-04 14:03:26 +0100

    feat: add logic workflow

---
+++ script.js
@@ -0,0 +1,9 @@
+//Procedimento logico
+/*
+1. Importo gli elementi di cui ho bisogno
+2. inizializzo
+3. Ogni qualvolta clicco il bottone chiamo l'api 10 volte
+4. Pusho le email ricevute in un array
+5. eseeguo una verifica se sono state eseguite tutte le chiamate
+6. se Si faccio comparire la classe d-none al loader e stamp le risposte in lista, se No continuo a pushare
+*/
